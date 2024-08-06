6c74ebdd48c71ace89455355ce4dc5c976ba5a90
Cristhian Cezar <cristhiancezardev@gmail.com>
2024-08-06 16:41:14 -0300

    respinsividade finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,3 +42,83 @@ for (const link of links) {
     nav.classList.remove("show");
   });
 }
+
+
+const vmRealiza = document.getElementById('vmRealiza');
+const stormtech = document.getElementById('stormtech');
+const rouxinol = document.getElementById('rouxinol');
+const racing = document.getElementById('racing');
+const bystorm = document.getElementById('bystorm');
+
+const bystormModal = document.getElementById('bystormModal');
+const vmModal = document.getElementById('vmModal');
+const rouxinolmModal = document.getElementById('rouxinolmModal');
+const racingmModal = document.getElementById('racingmModal');
+const stormModal = document.getElementById('stormModal');
+
+const closeStormModal = document.getElementById('closeStormModal')
+const closeRacingmModal = document.getElementById('closeRacingmModal')
+const closeRouxinolModal = document.getElementById('closeRouxinolModal')
+const closeVmModal = document.getElementById('closeVmModal')
+const closeBystormModal = document.getElementById('closeBystormModal')
+
+function closeVm () {
+  vmModal.style.display = "none";
+}
+
+function openModalVm() {
+  vmModal.style.display = "flex";
+}
+
+
+function closeBystorm () {
+  bystormModal.style.display = "none";
+}
+
+function openModalBystorm() {
+  bystormModal.style.display = "flex";
+}
+
+
+function closeRacing () {
+  racingmModal.style.display = "none";
+}
+
+function openModalRacing() {
+  racingmModal.style.display = "flex";
+}
+
+
+function closeRouxinol () {
+  rouxinolmModal.style.display = "none";
+}
+
+function openModalRouxinol() {
+  rouxinolmModal.style.display = "flex";
+}
+
+
+function closeStorm () {
+  stormModal.style.display = "none";
+}
+
+function openModalStorm() {
+  stormModal.style.display = "flex";
+}
+
+// Events
+
+closeVmModal.addEventListener('click', closeVm)
+closeStormModal.addEventListener('click', closeStorm)
+closeRacingmModal.addEventListener('click', closeRacing)
+closeRouxinolModal.addEventListener('click', closeRouxinol)
+closeBystormModal.addEventListener('click', closeBystorm)
+
+
+
+vmRealiza.addEventListener('click', openModalVm)
+stormtech.addEventListener('click', openModalStorm)
+rouxinol.addEventListener('click', openModalRouxinol)
+racing.addEventListener('click', openModalRacing)
+bystorm.addEventListener('click', openModalBystorm)
+
